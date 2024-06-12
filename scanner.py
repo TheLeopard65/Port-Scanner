d7d1276c72c7a70810eff1d11a575490d82bed77
Yasir Mehmood <152008995+TheLeopard65@users.noreply.github.com>
2024-06-12 19:21:59 +0500

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+import sys, socket
+from datetime import datetime
+
+def banner():
+    print("#" * 35)
+    print("TARGET (TEXT) : " + sys.argv[1])
+    print("TARGET (IP): " + target)
+    print("START TIME : " + str(datetime.now()))
+    print("#" * 35)
+
+def scan(target):
+    try:
+        for port in range(0,256):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.timeout()
+            result = s.connect_ex((target, port))
+            if result == 0:
+                print(f"PORT {port} : OPEN ✅")
+            elif result == 1:
+                print(f"PORT {port} : CLOSED ❌")
+            s.close()
+        
+    except KeyboardInterrupt:
+        print("Exiting Scanner")
+        sys.exit()
+        
+    except socket.gaierror:
+        print("ERROR : HOST could bot be Resolved")
+        sys.exit()
+        
+    except socket.error:
+        print("ERROR : Could not connect to the Target")
+        sys.exit()
+        
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("SYNTAX : python3 scanner.py <target>")
+    else:
+        target = socket.gethostbyname(sys.argv[1])
+        banner()
+        scan(target)
